eaf9aafb1d22babf2a1ae4bfdd5a7e03302ae1ca
Srijan Sarmah <srijan9864@gmail.com>
2024-08-19 02:01:23 +0530

    replaced - with space

--- PyramidFull.java
+++ PyramidFull.java
@@ -6,13 +6,13 @@ public class PyramidFull{
             int n = scanner.nextInt();
             for( int i = 0; i<n ; i++) {
                 for( int k = n - 1 ; k>i ; k--) {
-                    System.out.print("-");
+                    System.out.print(" ");
                 }
                 for( int j = 0; j< 2*i + 1 ; j++) {
                     System.out.print("*");
                 }
                 for( int l = n - 1; l>i ; l--) {
-                    System.out.print("-");
+                    System.out.print(" ");
                 }
                 System.out.println();
             }
