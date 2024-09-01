ca7514ab5031ed7b6253f7e1e3c04d81ac1b9a0e
Srijan Sarmah <srijan9864@gmail.com>
2024-09-01 22:05:47 +0530

    RecursionPrint

---
+++ RecursionPrint.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class RecursionPrint {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            int N = scanner.nextInt();
+            printNos(N);
+        }
+    }
+    public static void printNos(int N)
+    {
+       if(N > 0) {
+       printNos(N-1);
+       
+       System.out.print(N+" ");
+       }
+    }
+}
